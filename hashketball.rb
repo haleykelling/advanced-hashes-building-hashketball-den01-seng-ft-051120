9557e5e1e65cccf966b1048a5d10f158b27c0bce
Haley Kelling <haley@kellings.com>
2020-04-23 19:21:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -57,43 +57,43 @@ def game_hash
       ]
     },
     away: {
-      team_name: "Brooklyn Nets",
-      colors: ["Black", "White"]
+      team_name: "Charlotte Hornets",
+      colors: ["Turquoise", "Purple"]
       players: [{
-        player_name:
-        number:
-        shoe:
-        points:
-        rebounds:
-        assists:
-        steals:
-        blocks:
-        slam_dunks: },
+        player_name: "Jeff Adrien"
+        number: 4
+        shoe: 18
+        points: 10
+        rebounds: 1
+        assists: 1
+        steals: 2
+        blocks: 7
+        slam_dunks: 2 },
         {
-        player_name:
-        number:
-        shoe:
-        points:
-        rebounds:
-        assists:
-        steals:
-        blocks:
-        slam_dunks: },
+        player_name: "Bismack Biyombo"
+        number: 0 
+        shoe: 16
+        points: 12
+        rebounds: 4
+        assists: 7
+        steals: 22
+        blocks: 15
+        slam_dunks: 10 },
         {
-        player_name:
-        number:
-        shoe:
-        points:
-        rebounds:
-        assists:
-        steals:
-        blocks:
-        slam_dunks: },
+        player_name: "Desagna Diop"
+        number: 2
+        shoe: 14
+        points: 24
+        rebounds: 12
+        assists: 12
+        steals: 4
+        blocks: 5
+        slam_dunks: 5 },
         {
-        player_name: "Mason Plumlee"
-        number: 1
-        shoe: 19
-        points: 26
+        player_name: "Ben Gordon"
+        number: 8
+        shoe: 15
+        points26
         rebounds: 11
         assists: 6
         steals: 3
